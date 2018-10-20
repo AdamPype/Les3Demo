bdad8bea6f7575a866482f0e7fa981784375ad61
AdamPype <adam.pype@student.howest.be>
2018-10-20 23:31:18 +0200

    pistol stuff

--- Assets/AimPistolBehaviour.cs
+++ Assets/AimPistolBehaviour.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class AimPistolBehaviour : StateMachineBehaviour {
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}                                                                                                                      
+    [HideInInspector] public Transform AimTarget;
+    private float _lerp = 0;
 
-	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}         
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}                                                                                                                      
 
-	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}         
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}         
 
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}        
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}         
 
-	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-	override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	    //hey adam van volgende week :-)
-	}
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}        
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, AimTarget.position);
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, AimTarget.position);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+
+        }
 }
--- Assets/Scripts/BasePlayerScript.cs
+++ Assets/Scripts/BasePlayerScript.cs
@@ -13,13 +13,19 @@ public class BasePlayerScript : MonoBehaviour {
     [SerializeField] private float _maximumXZVelocity = (30 * 1000) / (60 * 60); //[m/s] 30km/h
     [SerializeField] private float _jumpHeight;
 
-    private Vector3 _velocity = Vector3.zero; // [m/s]
-    private Vector3 _inputMovement;
-    private bool _jump;
-
     private Transform _absoluteTransform;
     private CharacterController _char;
     private Animator _anim;
+    private Transform _aimPivot;
+    private Transform _aimTarget;
+
+    [HideInInspector] public Vector3 Velocity = Vector3.zero; // [m/s]
+    [HideInInspector] public Vector3 InputMovement;
+    private bool _jump;
+    private bool _isAiming = false;
+
+    private Vector3 _aim;
+
 
     void Start ()
         {
@@ -27,6 +33,10 @@ public class BasePlayerScript : MonoBehaviour {
         _char = GetComponent<CharacterController>();
         _absoluteTransform = Camera.main.transform;
         _anim = transform.GetChild(0).GetComponent<Animator>();
+        _aimPivot = transform.GetChild(1);
+        _aimTarget = _aimPivot.GetChild(0);
+
+        _anim.GetBehaviour<AimPistolBehaviour>().AimTarget = _aimTarget;
 
         //dependency error
         #if DEBUG
@@ -37,10 +47,16 @@ public class BasePlayerScript : MonoBehaviour {
 
     private void Update()
         {
-        _inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;  //.normalized;
+        InputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;  //.normalized;
+        _aim = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         if (Input.GetButtonDown("Jump"))
             {
             _jump = true;
+            }
+
+        if (Input.GetButtonDown("Fire1"))
+            {
+            _isAiming = !_isAiming;
             }
 
         }
@@ -54,13 +70,34 @@ public class BasePlayerScript : MonoBehaviour {
         ApplyJump();
         LimitXZVelocity();
 
-        Vector3 XZvel = Vector3.Scale(_velocity, new Vector3(1, 0, 1));
+        Vector3 XZvel = Vector3.Scale(Velocity, new Vector3(1, 0, 1));
         Vector3 localVelXZ = gameObject.transform.InverseTransformDirection(XZvel);
         _anim.SetFloat("VerticalVelocity", (localVelXZ.z * (_drag)) / _maximumXZVelocity);
         _anim.SetFloat("HorizontalVelocity", (localVelXZ.x * (_drag)) / _maximumXZVelocity);
+        _anim.SetBool("Aim", _isAiming);
+
+        if (_aim.magnitude > 0)
+            {
+            Vector3 relativeRotation = RelativeDirection(_aim);
+            _aimPivot.rotation = Quaternion.LookRotation(relativeRotation);
+
+            _aimPivot.localRotation = Quaternion.Euler(
+                                                _aimPivot.localRotation.eulerAngles.x, 
+                                                ClampAngle(_aimPivot.localRotation.eulerAngles.y, -90, 90), 
+                                                _aimPivot.localRotation.eulerAngles.z);
+            }
 
 
         DoMovement();
+        }
+
+    private Vector3 RelativeDirection(Vector3 direction)
+        {
+        //get relative rotation from camera
+        Vector3 xzForward = Vector3.Scale(_absoluteTransform.forward, new Vector3(1, 0, 1));
+        Quaternion relativeRot = Quaternion.LookRotation(direction);
+
+        return relativeRot.eulerAngles;
         }
 
 
@@ -69,7 +106,7 @@ public class BasePlayerScript : MonoBehaviour {
         if (_char.isGrounded)
             {
             //ground velocity
-            _velocity -= Vector3.Project(_velocity, Physics.gravity);
+            Velocity -= Vector3.Project(Velocity, Physics.gravity);
             }
         }
 
@@ -78,7 +115,7 @@ public class BasePlayerScript : MonoBehaviour {
         if (!_char.isGrounded)
             {
             //apply gravity
-            _velocity += Physics.gravity * Time.deltaTime;
+            Velocity += Physics.gravity * Time.deltaTime;
             }
         }
 
@@ -91,20 +128,20 @@ public class BasePlayerScript : MonoBehaviour {
             Quaternion relativeRot = Quaternion.LookRotation(xzForward);
 
             //move in relative direction
-            Vector3 relativeMov = relativeRot * _inputMovement;
-            _velocity += relativeMov * _acceleration * Time.deltaTime;
+            Vector3 relativeMov = relativeRot * InputMovement;
+            Velocity += relativeMov * _acceleration * Time.deltaTime;
             }
 
         }
 
     private void LimitXZVelocity()
         {
-        Vector3 yVel = Vector3.Scale(_velocity, Vector3.up);
-        Vector3 xzVel = Vector3.Scale(_velocity, new Vector3(1, 0, 1));
+        Vector3 yVel = Vector3.Scale(Velocity, Vector3.up);
+        Vector3 xzVel = Vector3.Scale(Velocity, new Vector3(1, 0, 1));
 
         xzVel = Vector3.ClampMagnitude(xzVel, _maximumXZVelocity);
 
-        _velocity = xzVel + yVel;
+        Velocity = xzVel + yVel;
         }
 
     private void ApplyDragOnGround()
@@ -112,7 +149,7 @@ public class BasePlayerScript : MonoBehaviour {
         if (_char.isGrounded)
             {
             //drag
-            _velocity = _velocity * (1 - _drag * Time.deltaTime); //same as lerp
+            Velocity = Velocity * (1 - _drag * Time.deltaTime); //same as lerp
             }
         }
 
@@ -120,7 +157,7 @@ public class BasePlayerScript : MonoBehaviour {
         {
         if (_char.isGrounded && _jump)
             {
-            _velocity.y += Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
+            Velocity.y += Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
             _jump = false;
             }
         }
@@ -128,7 +165,49 @@ public class BasePlayerScript : MonoBehaviour {
     private void DoMovement()
         {
         //do velocity / movement on character controller
-        Vector3 movement = _velocity * Time.deltaTime;
+        Vector3 movement = Velocity * Time.deltaTime;
         _char.Move(movement);
+        }
+
+    public static float ClampAngle(float angle, float min, float max)
+        {
+        angle = Mathf.Repeat(angle, 360);
+        min = Mathf.Repeat(min, 360);
+        max = Mathf.Repeat(max, 360);
+        bool inverse = false;
+        var tmin = min;
+        var tangle = angle;
+        if (min > 180)
+            {
+            inverse = !inverse;
+            tmin -= 180;
+            }
+        if (angle > 180)
+            {
+            inverse = !inverse;
+            tangle -= 180;
+            }
+        var result = !inverse ? tangle > tmin : tangle < tmin;
+        if (!result)
+            angle = min;
+
+        inverse = false;
+        tangle = angle;
+        var tmax = max;
+        if (angle > 180)
+            {
+            inverse = !inverse;
+            tangle -= 180;
+            }
+        if (max > 180)
+            {
+            inverse = !inverse;
+            tmax -= 180;
+            }
+
+        result = !inverse ? tangle < tmax : tangle > tmax;
+        if (!result)
+            angle = max;
+        return angle;
         }
     }
